57ebd9eada301901c76664533d9e5399c271f9ef
Armando <armandoandrecalderon@gmail.com>
2020-08-07 18:16:38 -0400

    lib/part_2.rb/no_valid_url

--- blocks_project/lib/part_2.rb
+++ blocks_project/lib/part_2.rb
@@ -1,3 +1,7 @@
 def all_words_capitalized?(words)
   words.all? { |word| word.capitalize == word }
 end
+
+def no_valid_url?(urls)
+  urls.none? { |ele| ['com', 'net', 'io', 'org'].include?(ele.split(".")[-1]) } 
+end
