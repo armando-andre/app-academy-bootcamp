581550595b5eb985fe1a585a640ae2386f551a6f
Armando <armandoandrecalderon@gmail.com>
2020-08-07 18:34:22 -0400

    lib/part_2.rb/any_passing_students

--- blocks_project/lib/part_2.rb
+++ blocks_project/lib/part_2.rb
@@ -5,3 +5,7 @@ end
 def no_valid_url?(urls)
   urls.none? { |ele| ['com', 'net', 'io', 'org'].include?(ele.split(".")[-1]) } 
 end
+
+def any_passing_students?(arr)
+  arr.any? { |ele| ele[:grades].sum / ele[:grades].length > 75 }
+end
