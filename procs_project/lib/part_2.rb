2c4d7d4f7956b0efa4d92da5658aa63dda1b0353
Armando <armandoandrecalderon@gmail.com>
2020-08-20 19:17:06 -0400

    lib/part_2.rb/word_changer

--- procs_project/lib/part_2.rb
+++ procs_project/lib/part_2.rb
@@ -1,3 +1,15 @@
 def reverser(str, &prc)
   prc.call(str.reverse { |ele| ele })
 end
+
+def word_changer(str, &prc)
+  newStr = []
+
+  str.split(" ").each do |ele|
+    newStr << prc.call(ele)
+  end
+
+  newStr.join(" ")
+end
+
+word_changer("goodbye moon") { |word| word.upcase + "!" }
