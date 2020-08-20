08ac21d937ef87c1351353d5f1208f46170bea09
Armando <armandoandrecalderon@gmail.com>
2020-08-19 22:21:47 -0400

    lib/part_1.rb/my_none?

--- procs_project/lib/part_1.rb
+++ procs_project/lib/part_1.rb
@@ -38,7 +38,7 @@ def my_any?(arr, &prc)
 end
 
 def my_all?(arr, &prc)
-    switch = nil
+  switch = nil
 
   arr.each do |val|
     if prc.call(val) === false
@@ -51,3 +51,20 @@ def my_all?(arr, &prc)
 
   switch
 end
+
+def my_none?(arr, &prc)
+  switch = nil
+
+  arr.each do |val|
+    if prc.call(val) === true
+      switch = false
+      break
+    else
+      switch = true
+    end
+  end
+
+  p switch
+end
+
+my_none?([3, 5, 7, 11]) { |n| n.even? }
