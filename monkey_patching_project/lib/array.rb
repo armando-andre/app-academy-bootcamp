6b904e77cea347c8e00aa15d704f990a8fb9cc10
Armando <armandoandrecalderon@gmail.com>
2020-10-03 13:34:34 -0400

    lib/array.rb/spann

--- monkey_patching_project/lib/array.rb
+++ monkey_patching_project/lib/array.rb
@@ -1,4 +1,12 @@
 # Monkey-Patch Ruby's existing Array class to add your own custom methods
 class Array
-  
+  def span
+    return nil if self.length === 0
+
+    if self.length > 1
+      return self.max - self.min
+    else
+      return 0
+    end      
+  end
 end
