83ec3fe6cd954fab7346bcf84c28ea15ceabbd54
Armando <armandoandrecalderon@gmail.com>
2020-10-20 18:50:09 -0400

    lib/room.rb/initialize

--- hotel_project/lib/hotel.rb
+++ hotel_project/lib/hotel.rb
@@ -1,5 +1,18 @@
 require_relative "room"
 
 class Hotel
-  
+  def initialize(string, hash)
+    
+    hash.each do |key, val|
+      @name = string
+      @rooms = hash.each do |key, val|
+        Hash.new(Room.new(self))
+      end
+
+      p @rooms
+    end
+  end
 end
+
+
+p Hotel.new("hilbert's grand hotel", "Basement"=>4)
