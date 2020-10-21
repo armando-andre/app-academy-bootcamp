d2a89744c4bb2ed9f0f85e438fe89b48e590baf0
Armando <armandoandrecalderon@gmail.com>
2020-10-21 18:59:33 -0400

    lib/hotel.rb/name

--- hotel_project/lib/hotel.rb
+++ hotel_project/lib/hotel.rb
@@ -2,17 +2,20 @@ require_relative "room"
 
 class Hotel
   def initialize(string, hash)
-    
-    hash.each do |key, val|
-      @name = string
-      @rooms = hash.each do |key, val|
-        Hash.new(Room.new(self))
-      end
+    @name =  string
+    @rooms = {}
 
-      p @rooms
+    hash.each do |key, val|
+      @rooms[key] = Room.new(val)
+    end
+
+    def name
+      upcaseName = @name.split(" ")
+
+      upcaseName.each { |ele| ele[0] = ele[0].upcase }.join(" ")
     end
   end
 end
 
 
-p Hotel.new("hilbert's grand hotel", "Basement"=>4)
+p Hotel.new("hilbert's grand hotel", "Basement"=>4).name
