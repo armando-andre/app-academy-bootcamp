59243def56ebcd9df40f9f1395ca3be1f5833665
Armando <armandoandrecalderon@gmail.com>
2020-10-28 17:57:10 -0400

    lib/startup.rb/initialize, name, funding, salaries, employees, valid_title?, >, hire, size

--- startup_project/lib/startup.rb
+++ startup_project/lib/startup.rb
@@ -1,5 +1,50 @@
 require "employee"
 
 class Startup
+  def initialize(name, funding, salaries)
+    @name = name
+    @funding  = funding
+    @salaries = salaries
+    @employees = []
+  end
 
+  def name
+    @name
+  end
+
+  def funding
+    @funding
+  end
+
+  def salaries
+    @salaries
+  end
+
+  def employees
+    @employees
+  end
+
+  def valid_title?(title)
+    @salaries.include?(title)
+  end
+
+  def >(startup)
+    if self.funding > startup.funding
+      true
+    else
+      false
+    end
+  end
+
+  def hire(employee_name, title)
+    if valid_title?(title)
+      @employees << Employee.new(employee_name, title)
+    else
+      raise StandardError.new false
+    end
+  end
+
+  def size
+    @employees.length
+  end
 end
